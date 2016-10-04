64db5d854da2f9ed9b670ff2c3330ce58318793e
Abiy Seifu <abiyzel@gmail.com>
2016-10-03 22:36:10 -0700

    [FEATURE] CLI

---
+++ src/cli/index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const peristenceManager = require('services/data/persistence_manager.js');
+const earthQuakesService = require('services/earthquakes');
+const syncService = require('services/sync');
+require('console.table');
+
+program
+.option('-l, --limit <limit>', 'Number of most dangerous to list (default=10)', parseInt)
+.option('-d, --days <days>', 'Number days to go back to analyze data from (default=30)', parseInt)
+.parse(process.argv);
+
+// ensure database is ready
+return peristenceManager.sync()
+.then(() => syncService.checkForEarthQuakeDataUpdates())
+.then(() => earthQuakesService.getEarthQuakes({
+  limit: program.limit,
+  number_of_days: program.days
+}))
+.then(console.table);
