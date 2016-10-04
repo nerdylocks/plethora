05b07c194335fa2289ef9ef71f70ad8b25bd07d4
Abiy Seifu <abiyzel@gmail.com>
2016-10-03 22:36:34 -0700

    [FEATURE] consolidate config

--- src/config/index.js
+++ src/config/index.js
@@ -12,8 +12,17 @@ nconf.defaults({
   base_data_source_url: 'http://earthquake.usgs.gov',
   data_source_endpoint: '/earthquakes/feed/v1.0/summary/all_month.geojson',
   last_generated: '',
-  latest_earthquake_record: '', // to avoid unnecessary database
-  google_api_key: ''
+  latest_earthquake_record: '', // to avoid unnecessary database,
+  database: {
+    driver: 'pg',
+    dialect: 'postgres',
+    database_name: 'earthquakes_db',
+    user: 'plethora_db_admin',
+    password: 'password',
+    host: 'localhost',
+    port: '5432',
+    logging: false
+  }
 });
 
 module.exports = config;
