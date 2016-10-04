af12ee7c55e0b8ad0f9dac0288966226b602c475
Abiy Seifu <abiyzel@gmail.com>
2016-10-04 14:43:17 -0700

    [TASK] clean up config properties, rename config.json to config-eample.json

--- config/index.js
+++ config/index.js
@@ -11,8 +11,6 @@ const config = nconf
 nconf.defaults({
   base_data_source_url: 'http://earthquake.usgs.gov',
   data_source_endpoint: '/earthquakes/feed/v1.0/summary/all_month.geojson',
-  last_generated: '',
-  latest_earthquake_record: '', // to avoid unnecessary database,
   database: {
     driver: 'pg',
     dialect: 'postgres',
